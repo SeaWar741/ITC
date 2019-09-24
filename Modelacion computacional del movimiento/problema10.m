ca5ddd2900ed1aae2dd286b918cfbb66b1f7e754
SeaWar741 <juanca741@gmail.com>
2019-09-24 14:47:00 -0500

    computacional del movimiento

---
+++ Modelacion computacional del movimiento/problema10.m
@@ -0,0 +1,6 @@
+%%Juan Carlos Garfias Tovar, A01652138
+%%Problema 10
+disp("Conversión de Fahrenheit a Celsius");
+tempf = input("Ingresar temperatura en grados F:");
+tempc = (5/9) * (tempf - 32);
+disp("La temperatura en grados C es:"+tempc);
