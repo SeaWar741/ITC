d6235daaa99bf698a85c801ea8d219e432cb500d
SeaWar741 <juanca741@gmail.com>
2019-10-06 16:03:10 -0500

    doing neural network for detecting lung cancer

--- Sistemas Biologicos/generateimage.py
+++ Sistemas Biologicos/generateimage.py
@@ -1,13 +1,143 @@
-import numpy as np
+from keras.callbacks import ModelCheckpoint
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error 
 from matplotlib import pyplot as plt
-import matplotlib
-import glob
+import seaborn as sb
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import warnings 
+warnings.filterwarnings('ignore')
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+from xgboost import XGBRegressor
+
+def get_data():
+    #get train data
+    train_data_path ='lung_cancer_examples.csv'
+    train = pd.read_csv(train_data_path)
+    
+    #get test data
+    test_data_path ='lung_cancer_examples.csv'
+    test = pd.read_csv(test_data_path)
+    
+    return train , test
+
+def get_combined_data():
+  #reading train data
+  train , test = get_data()
+
+  target = train.Result
+  train.drop(['Result'],axis = 1 , inplace = True)
+
+  combined = train.append(test)
+  combined.reset_index(inplace=True)
+  combined.drop(['index', 'Name'], inplace=True, axis=1)
+  return combined, target
+
+#Load train and test data into pandas DataFrames
+train_data, test_data = get_data()
+
+#Combine train and test data to process them together
+combined, target = get_combined_data()
+
+def get_cols_with_no_nans(df,col_type):
+    '''
+    Arguments :
+    df : The dataframe to process
+    col_type : 
+          num : to only get numerical columns with no nans
+          no_num : to only get nun-numerical columns with no nans
+          all : to get any columns with no nans    
+    '''
+    if (col_type == 'num'):
+        predictors = df.select_dtypes(exclude=['object'])
+    elif (col_type == 'no_num'):
+        predictors = df.select_dtypes(include=['object'])
+    elif (col_type == 'all'):
+        predictors = df
+    else :
+        print('Error : choose a type (num, no_num, all)')
+        return 0
+    cols_with_no_nans = []
+    for col in predictors.columns:
+        if not df[col].isnull().any():
+            cols_with_no_nans.append(col)
+    return cols_with_no_nans
 
 
-for filename in glob.glob("*.*"):
-    if '.npy' in filename:
-        img_array = np.load(filename)
-        plt.imshow(img_array, cmap="gray")
-        img_name = filename+".png"
-        matplotlib.image.imsave(img_name, img_array)
-        print(filename)
+num_cols = get_cols_with_no_nans(combined , 'num')
+cat_cols = get_cols_with_no_nans(combined , 'no_num')
+
+
+print ('Number of numerical columns with no nan values :',len(num_cols))
+print ('Number of nun-numerical columns with no nan values :',len(cat_cols))
+
+combined = combined[num_cols + cat_cols]
+combined.hist(figsize = (12,10))
+plt.show()
+
+train_data = train_data[num_cols + cat_cols]
+train_data['Target'] = target
+
+C_mat = train_data.corr()
+fig = plt.figure(figsize = (15,15))
+
+sb.heatmap(C_mat, vmax = .8, square = True)
+plt.show()
+
+def oneHotEncode(df,colNames):
+    for col in colNames:
+        if( df[col].dtype == np.dtype('object')):
+            dummies = pd.get_dummies(df[col],prefix=col)
+            df = pd.concat([df,dummies],axis=1)
+
+            #drop the encoded column
+            df.drop([col],axis = 1 , inplace=True)
+    return df
+    
+
+print('There were {} columns before encoding categorical features'.format(combined.shape[1]))
+combined = oneHotEncode(combined, cat_cols)
+print('There are {} columns after encoding categorical features'.format(combined.shape[1]))
+
+def split_combined():
+    global combined
+    train = combined[:1460]
+    test = combined[1460:]
+
+    return train , test 
+  
+train, test = split_combined()
+
+##Neural network
+
+NN_model = Sequential()
+
+# The Input Layer :
+NN_model.add(Dense(128, kernel_initializer='normal',input_dim = train.shape[1], activation='relu'))
+
+# The Hidden Layers :
+NN_model.add(Dense(256, kernel_initializer='normal',activation='relu'))
+NN_model.add(Dense(256, kernel_initializer='normal',activation='relu'))
+NN_model.add(Dense(256, kernel_initializer='normal',activation='relu'))
+
+# The Output Layer :
+NN_model.add(Dense(1, kernel_initializer='normal',activation='linear'))
+
+# Compile the network :
+NN_model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
+NN_model.summary()
+
+checkpoint_name = 'Weights-{epoch:03d}--{val_loss:.5f}.hdf5' 
+checkpoint = ModelCheckpoint(checkpoint_name, monitor='val_loss', verbose = 1, save_best_only = True, mode ='auto')
+callbacks_list = [checkpoint]
+
+NN_model.fit(train, target, epochs=500, batch_size=32, validation_split = 0.2, callbacks=callbacks_list)
+
+# Load wights file of the best model :
+wights_file = 'Weights-478--18738.19831.hdf5' # choose the best checkpoint 
+NN_model.load_weights(wights_file) # load it
+NN_model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
