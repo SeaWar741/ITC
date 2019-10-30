2135bdabc8bfd3efe7d511162886adef601fc83c
SeaWar741 <juanca741@gmail.com>
2019-10-29 23:46:41 -0600

    progress on df for spark

--- Competencias/Spark/Join.py
+++ Competencias/Spark/Join.py
@@ -5,11 +5,12 @@ import datetime
 
 # Read in your file as a pandas.DataFrame
 # using 'any number of whitespace' as the seperator
-df = pd.read_csv("./Cerro_prieto.csv")
-
+df = pd.read_csv("./Cerro_prieto.csv",parse_dates=['Date'],index_col=0)
 print(df)
 print(df.head())
 
 #df.resample('M').mean()
-df.groupby(df.index.Date).mean()
-print(df)
+df = df.fillna(df.mean())
+monthly=df.resample('M', how='mean')
+#df.groupby(df.index.Date).mean()
+print(monthly)
