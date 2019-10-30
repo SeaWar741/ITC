8a12aa8545d9772480b25c5cbcceb03ea34062dd
SeaWar741 <juanca741@gmail.com>
2019-10-30 00:03:31 -0600

    script done

--- Competencias/Spark/Join.py
+++ Competencias/Spark/Join.py
@@ -7,10 +7,13 @@ import datetime
 # using 'any number of whitespace' as the seperator
 df = pd.read_csv("./Cerro_prieto.csv",parse_dates=['Date'],index_col=0)
 print(df)
-print(df.head())
 
+df[df.columns] = df[df.columns].apply(pd.to_numeric, errors='coerce')
 #df.resample('M').mean()
-df = df.fillna(df.mean())
+print("Replacing nan")
+df.fillna(df.mean(), inplace=True)
+print(df)
 monthly=df.resample('M', how='mean')
 #df.groupby(df.index.Date).mean()
 print(monthly)
+df.to_csv(r'./Data_retreived.csv')
