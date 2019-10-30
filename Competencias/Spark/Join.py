c3f5eb83c41ae11a9586780ed0cf50d61f4f6df0
SeaWar741 <juanca741@gmail.com>
2019-10-30 00:38:39 -0600

    script 100% working, important to change datatype on csv, change date time and fix data before giving it to the script

--- Competencias/Spark/Join.py
+++ Competencias/Spark/Join.py
@@ -2,21 +2,30 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
+import glob
 
-# Read in your file as a pandas.DataFrame
-# using 'any number of whitespace' as the seperator
-df = pd.read_csv("./Cerro_prieto.csv",parse_dates=['Date'],index_col=0)
-print(df)
+counter = 0
 
-df[df.columns] = df[df.columns].apply(pd.to_numeric, errors='coerce')
-#df.resample('M').mean()
-print("Replacing nan")
-df.fillna(df.mean(), inplace=True)
-print(df)
-monthly=df.resample('M', how='mean')
-#df.groupby(df.index.Date).mean()
-print(monthly)
+for filen in glob.glob("./"+'*.csv'):
+    # Read in your file as a pandas.DataFrame
+    # using 'any number of whitespace' as the seperator
+    print(f"Iniciando {filen}")
+    df = pd.read_csv(f"./{filen}",parse_dates=['Date'],index_col=0)
+    print(df)
 
-df['T.Media'] = df.apply(lambda row: ((row.TMAX + row.TMIN)/2), axis = 1) 
+    df[df.columns] = df[df.columns].apply(pd.to_numeric, errors='coerce')
+    #df.resample('M').mean()
+    print("Replacing nan")
+    df.fillna(df.mean(), inplace=True)
+    print(df)
+    monthly=df.resample('M', how='mean')
+    #df.groupby(df.index.Date).mean()
+    print(monthly)
 
-df.to_csv(r'./Data_retreived.csv')
+    df['T.Media'] = df.apply(lambda row: ((row.TMAX + row.TMIN)/2), axis = 1) 
+    filen= filen[:-4]
+    nfile = f"{filen}_processed_data.csv"
+
+    df.to_csv(nfile)
+
+    print(f"Se completo el archivo {filen}| Counter: {counter}")
