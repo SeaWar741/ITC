dab0e4d825f0d4705b91ee3b591cdc5910296c56
SeaWar741 <juanca741@gmail.com>
2019-11-14 02:04:29 -0600

    tarea/avance del reto completado

--- 1er_Semestre/Pensamiento_computacional_orientado_a_objetos/tarea_datos_estructurados.cpp
+++ 1er_Semestre/Pensamiento_computacional_orientado_a_objetos/tarea_datos_estructurados.cpp
@@ -15,111 +15,140 @@
 #include <sstream>
 using namespace std;
 
-const int N = 10;
-const int M = 8;
+const int N = 10;//Columnas
+const int M = 8;//Renglones
 
+//Nombre:despliegaMatriz
+//Descripcion: Metodo para imprimir una matriz
+//              
+//Entrada:
+//          Matriz deseada
+//Salida:
+//          imprime una matriz en formato de tabla
 void despliegaMatriz(int arry[][M]){
     cout<<endl;
-    for (int i = 0; i < M; i++){
+    for (int i = 0; i < M; i++){ //itera sobre la matriz
         for (int j = 0; j < N; j++){
-            cout<<arry[i][j]<<" ";
+            cout<<arry[i][j]<<"\t";//utiliza un tab para igualar las distancias al imprimir
         }
-        cout<<endl;
+        cout<<endl;//salto de linea cada fila completada
     }
     cout<<endl;
 }
 
+//Nombre:llenaConArchivo
+//Descripcion: Metodo para leer datos de un archivo y almacenarlos en una
+//              matriz dada
+//Entrada:
+//          Matriz deseada y nombre del archivo
+//Salida:
+//          Añade los datos a la matriz, en caso de que sean menores al tamaño se reemplazan por 0
 void llenaConArchivo(int arry[][M],string txt){
     cout<<endl;
-    ifstream inFile;
-    string pathf = "./"+txt+".txt";
+    ifstream inFile; //se crea el ifstream
+    string pathf = "./"+txt+".txt"; //se hace un append para que lea el archivo en el directorio actual y le añade la extension
     int x,counter;
-    inFile.open(pathf);
+    inFile.open(pathf); //abre el archivo desde el path
     if (!inFile) {
-        cerr << "No se pudo abrir el archivo "<<txt<<endl;
+        cerr << "No se pudo abrir el archivo "<<txt<<endl; //si no se encuentra el archivo
     }
     else{
         string str;
         int number;
         int ncounter =0;
         int counter=0;
-        int readn[80] ={0};
-        while (std::getline(inFile, str) && ncounter <=80) {
-            std::istringstream iss (str);
+        int readn[80] ={0}; //se inicializa el array con 0 en todos los elementos
+        while (std::getline(inFile, str) && ncounter <=80) { //se lee cada linea mientras que no pase de 80 elementos
+            std::istringstream iss (str); //se convierte a int
             iss >> number;
-            readn[ncounter] = number;
-            ncounter++;
+            readn[ncounter] = number;//se establece el valor
+            ncounter++; //aumenta el contador de lineas
         }
         for (int i = 0; i < M; i++){
             for (int j = 0; j < N; j++){
-                arry[i][j]=readn[counter];
+                arry[i][j]=readn[counter]; //se establecen los valores leidos en la matriz deseada deseado
                 counter++;
             }
         }
     }
-    inFile.close();
+    inFile.close(); //se cierra el archivo
 }
 
+//Nombre:llenaConAleatorios
+//Descripcion: Metodo para asignar valores aleatorios a una
+//              matriz dada
+//Entrada:
+//          Matriz deseada 
+//Salida:
+//          Añade los datos a la matriz en un rango especifico
 void llenaConAleatorios(int arry[][M]){
-    //. Llena la matriz que recibe como parámetro con números aleatorios
-    //dentro del rango de 0 a 1500.
     srand(time(0));   //se inicializa el random
     int value = 0;
     for (int i = 0; i < N; i++){
         for (int j = 0; j < M; j++){
-            value = rand() % 1500 + 1;
-            arry[i][j] = value;
+            value = rand() % 1500 + 1;//crea un valor aleatorio en el rango
+            arry[i][j] = value;//lo establece de manera iterativa
         }
     }
 }
 
+//Nombre:sumaMatrices
+//Descripcion: Suma matrices dadas
+//Entrada:
+//          Matrices deseadas y matriz para almacenar suma
+//Salida:
+//          Realiza una suma a las matrices, almacena el resultado en un archivo de texto
 void sumaMatrices(int arry[][M], int arry2[][M],int sum[][M]){
-    /*Calcula la suma de dos matrices que recibe como parámetro (A y B) y
-    coloca el resultado en una tercera matriz (C) que también se recibe como parámetro.
-    Después de realizar la suma de las matrices A y B, el contenido de la matriz C debe
-    almacenarse en el archivo salida.txt en formato "matriz", es decir con N renglones y M
-    columnas. El contenido del archivo salida.txt no debe reescribirse por cada ejecución del
-    método, el nuevo contenido debe agregarse al final del archivo.*/
     for(int i = 0; i < N; ++i){
         for(int j = 0; j < M; ++j){
-            sum[i][j] = arry[i][j] + arry2[i][j];
+            sum[i][j] = arry[i][j] + arry2[i][j]; //realiza la sumatoria y lo almacena en el nuevo array
         }
     }
-    fstream myfile;
-    myfile.open("salida.txt",fstream::app);
+    fstream myfile; //se llama a fstram
+    myfile.open("salida.txt",fstream::app);//se establece que se hara un append al archivo
     for (int i=0; i< M;i++){        
         for (int j=0; j<N;j++){                      
-            myfile << sum[i][j] <<"\t";
+            myfile << sum[i][j] <<"\t";//se añade cada elemento con un tab para espaciado
         }
-        myfile<<std::endl;
+        myfile<<std::endl;//se realiza un salto de linea cada row
     }
-    myfile.close();
+    myfile.close();//se cierra el documento/archivo
     cout<<"-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
     cout<<"Se guardo la suma en salida.txt exitosamente"<<endl;
     cout<<"-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
 }
 
+//Nombre:despliegaDiagonal
+//Descripcion: Suma matrices dadas
+//Entrada:
+//          Matriz deseada
+//Salida:
+//          Imprime la diagonal de la matriz dada
 void despliegaDiagonal(int arry[][M]){
-     /*Muestra en pantalla la diagonal principal de la matriz que recibe
-     como parámetro. Se pide que la diagonal se muestre en el siguiente formato:*/
     cout<<"Diagonal del array :"<<endl;
-    for(int i=0 ; i<N &&i< M ; i++){
-        if(i<N-1){
-            cout<<arry[i][i]<<"  ";
+    for(int i=0 ; i<N &&i< M ; i++){//mientras que sea menor a n y a M realiza el ciclo
+        for(int j = 0; j<i;j++){
+            cout<<"\t";//formato deseado, realiza el espaciado    
         }
-        else{
-            cout<<arry[i][i]<<endl;
-        }
+        cout<<arry[i][i]<<endl;
     }
 }
 
+//Nombre:intercambioaRenglonColumna
+//Descripcion: Realiza un intercambio del renglon con columna
+//Entrada:
+//          Matriz deseada
+//Salida:
+//          Sustituye el renglon con la columna, lo hace con la restriccion de el numero de filas por 
+//          lo que el ultimo elemento de la nueva sera el anterior ya que no se puede sustituir por el
+//          tamaño del elemento deseado
 void intercambioaRenglonColumna(int arry[][M]){
     int row,col,rowd,cold;
-    cout<<"\nIngresa el numero de renglon: >";cin>>row;cout<<endl;
+    cout<<"\nIngresa el numero de renglon: >";cin>>row;cout<<endl;//se ingresa el renglon y columna deseado
     cout<<"\nIngresa el numero de columna: >";cin>>col;cout<<endl;
-    if(row<=M&&col<=N && row>=1&&col>=1){
-        for(int i=0; i<M;i++){
-            rowd = arry[row-1][i];
+    if(row<=M&&col<=N && row>=1&&col>=1){//se realiza si se cumplen las condiciones del rango en la matriz
+        for(int i=0; i<M;i++){//se hace el intercambio de renglon columna
+            rowd = arry[row-1][i];//se hace el menos uno para que el usuario pueda hacer la seleccion desde 1
             cold = arry[i][col-1];
             
             arry[row-1][i]=cold;
@@ -127,7 +156,7 @@ void intercambioaRenglonColumna(int arry[][M]){
         }
         cout<<"\nSe realizo el intercambio!"<<endl;
     }
-    else{
+    else{//si no se cumple se imprime donde estuvo el error o si fue en ambos
         if(row>M||row<1){
             cout<<"Renglon fuera de rango\n";
         }
@@ -137,30 +166,42 @@ void intercambioaRenglonColumna(int arry[][M]){
     }
 }
 
+//Nombre:selection
+//Descripcion: char para obtener la seleccion del usuario de la matriz deseada
+//Entrada:
+//          
+//Salida:
+//          regresa un char en minusculas de la seleccion deseada para ejecutar el metodo
 char selection(){
     char selection;
     cout<<"Ingresar letra de matriz(a/b/c): >";cin>>selection;
-    selection = tolower(selection);
+    selection = tolower(selection); //se pasa a minusculas
     cout<<endl;
-    return selection;
+    return selection;//se regresa el char
 }
 
+//Nombre:copyMatrix
+//Descripcion: Se realiza una copia de la matriz
+//Entrada:
+//          matriz a copiar y a ser copiada
+//Salida:
+//          le establece los valores de la matriz a la otra
 void copyMatrix(int arry[][M], int arry2[][M]){ 
     for (int i = 0; i < M; i++){
         for (int j = 0; j < N; j++){
-            arry[i][j]=arry2[i][j];
+            arry[i][j]=arry2[i][j]; //se establecen los indices con los valores de la anterior
         }
     }
 }
 
 int main(){
-    int matrizA[N][M] ={0},matrizB[N][M]={0},matrizC[N][M]={0},sum[N][M]={0};
-    char option,keep,matrixsave,suboption,matrixa,matrixb;
-    string txt;
-    bool continueg = true;
+    int matrizA[N][M] ={0},matrizB[N][M]={0},matrizC[N][M]={0},sum[N][M]={0};//se inicializan las matrices en 0
+    char option,keep,matrixsave,suboption,matrixa,matrixb;//se inicializan los char para los menus
+    string txt;//variable para el nombre de archivo
+    bool continueg = true;//variable para controlar las repeticiones del menu
     cout<<endl;
     cout<<"-*-*-*-*-*-*-*-*-*-*-*-*-=Bienvenid@ a la tarea de datos estructurados=-*-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
-    while(continueg){
+    while(continueg){//loop mientras que el usuario no seleccione la opcion de salir
         cout<<"\na) Llenar matriz con archivo"<<endl;
         cout<<"b) Llenar matriz con numeros aleatorios"<<endl;
         cout<<"c) Sumar Matrices"<<endl;
@@ -170,18 +211,18 @@ int main(){
         cout<<endl;
         cout<<"Matrices:"<<endl;
         cout<<"Matriz A:";
-        despliegaMatriz(matrizA);
+        despliegaMatriz(matrizA);//se despliegan las matrices
         cout<<"Matriz B:";
         despliegaMatriz(matrizB);
         cout<<"Matriz C:";
         despliegaMatriz(matrizC);
         cout<<endl;
-        cout<<"Ingresa una opcion del menu: >";cin>>option;
-        switch (option){
-            case 'a':
-                cout<<"\nIngresa el nombre del archivo: >";cin>>txt;
+        cout<<"Ingresa una opcion del menu: >";cin>>option;//se pide el input del usuario
+        switch (option){//switch principal para las opciones del menu
+            case 'a'://establecer datos de la matriz con archivo
+                cout<<"\nIngresa el nombre del archivo: >";cin>>txt;//se pide el nombre
                 cout<<endl;
-                suboption = selection();
+                suboption = selection();//se pide a que matriz se le asignaran los valores y se realiza la funcion
                 if(suboption == 'a'){
                     llenaConArchivo(matrizA,txt);
                 }
@@ -193,8 +234,8 @@ int main(){
                 }
                 cout<<"-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
                 break;
-            case 'b':
-                suboption = selection();
+            case 'b'://establecer valores aleatorios 
+                suboption = selection(); //se pide la seleccion de la matriz al usuario y se realiza la funcion
                 if(suboption == 'a'){
                     llenaConAleatorios(matrizA);
                 }
@@ -206,10 +247,10 @@ int main(){
                 }
                 cout<<"-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
                 break;
-            case 'c':
-                matrixa=selection();
-                matrixb=selection();
-                if(matrixa == 'a' &&matrixb=='b'|| matrixa == 'b' &&matrixb=='a'){
+            case 'c'://suma de matrices con matrices dadas
+                matrixa=selection();//se pide la matriz 1
+                matrixb=selection();//se pide la matriz 2
+                if(matrixa == 'a' &&matrixb=='b'|| matrixa == 'b' &&matrixb=='a'){//condicionales para todos los posibles casos
                     sumaMatrices(matrizA,matrizB,sum);
                 }
                 else if(matrixa == 'a' &&matrixb=='c'||matrixa == 'c' &&matrixb=='a'){
@@ -227,19 +268,19 @@ int main(){
                 else if(matrixa == 'c' && matrixb=='c'){
                     sumaMatrices(matrizC,matrizC,sum);
                 }
-                else{
+                else{//en caso de que no sea valida la combinacion se hace suma de a con b
                     cout<<"No se selecciono una combinacion valida, sumando matriz A con matriz B"<<endl;
                     sumaMatrices(matrizA,matrizB,sum);
                 }
 
                 cout<<"\nLa suma de las matrices es:"<<endl;
-                despliegaMatriz(sum);
+                despliegaMatriz(sum);//se imprime el valor de la suma
                 cout<<"Desea guardar la matriz en otra? (s/n): >";cin>>keep;cout<<endl;
-                if(tolower(keep)=='s'){
+                if(tolower(keep)=='s'){//si el usuario quiere guardar la matriz se pide que ingrese en donde guardarla
                     matrixsave = selection();
-                    switch (matrixsave){
+                    switch (matrixsave){//switch para seleccionar donde establecer los valores y copiar ahi la matriz
                     case 'a':
-                        copyMatrix(matrizA,sum);
+                        copyMatrix(matrizA,sum);//se llama al metodo
                         break;
                     case 'b':
                         copyMatrix(matrizB,sum);
@@ -248,15 +289,15 @@ int main(){
                         copyMatrix(matrizC,sum);
                         break;
                     default:
-                        copyMatrix(matrizA,sum);
-                        cout<<"Se guardo en la matriz a\n";
+                        copyMatrix(matrizA,sum);//en caso default se copia en A
+                        cout<<"Se guardo en la matriz A\n";
                     }
                 }
                 cout<<"-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
                 break;
-            case 'd':
+            case 'd'://Desplegar la diagonal de una matriz seleccionada
                 cout<<endl;
-                suboption = selection();
+                suboption = selection();//se pide la matriz y se realiza sobre ella el metodo
                 if(suboption == 'a'){
                     despliegaDiagonal(matrizA);
                 }
@@ -269,8 +310,8 @@ int main(){
                 cout<<endl;
                 cout<<"-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
                 break;
-            case 'e':
-                suboption = selection();
+            case 'e'://intercambio de renglon a columna 
+                suboption = selection();//se pide la matriz y se llama al metodo
                 if(suboption == 'a'){
                     intercambioaRenglonColumna(matrizA);
                 }
@@ -282,16 +323,16 @@ int main(){
                 }
                 cout<<"-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
                 break;
-            case 'f':
+            case 'f'://se termina el loop
                 continueg = false;
                 cout<<"-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
                 break;
-            default:
+            default://se pide al usuario en caso de un input no valido intentar de nuevo
                 cout<<"\nOpcion no valida, intentar de nuevo"<<endl;
                 cout<<"-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
                 break;
         }
     }
-    cout<<"\nHasta luego!\n";
+    cout<<"\nHasta luego!\n";//se acaba el programa
     return 0;
 }
