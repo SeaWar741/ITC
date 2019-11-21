ddf1c3eecd6a1a566a865b65e606669534c637f9
SeaWar741 <juanca741@gmail.com>
2019-11-21 12:59:52 -0600

    finished eventos.h

--- 1er_Semestre/Pensamiento_computacional_orientado_a_objetos/Eventos.h
+++ 1er_Semestre/Pensamiento_computacional_orientado_a_objetos/Eventos.h
@@ -18,13 +18,45 @@ public:
     RelojD getHora();//composicion
     int getDuracion();
     int getMin();
+    string getAsunto();
 
     void incrementaMinutos();
-    string print();
+    void print();
 };
 
 Eventos::Eventos(){
+    RelojD rolex(0,0);
+
+    hr=rolex;
+    iDuracionMin = 100;
+    sAsunto="Borregos Campeones";
 }
 
-Eventos::Eventos(){
+Eventos::Eventos(RelojD rolex, int iDuracion,string sAsunto){
+    hr=rolex;
+    iDuracionMin=iDuracion;
+    this->sAsunto=sAsunto;
+}
+
+void Eventos::setHora(RelojD rolex){
+    hr = rolex;
+}
+void Eventos::setDuracion(int iDuracion){
+    iDuracionMin=iDuracion;
+}
+void Eventos::setAsunto(string sAsunto){
+    this->sAsunto=sAsunto;
+}
+
+RelojD Eventos::getHora(){
+    return hr;
+}
+int Eventos::getDuracion(){
+    return iDuracionMin;
+}
+string Eventos::getAsunto(){
+    return sAsunto;
+}
+void Eventos::print(){
+    hr.desplegar();
 }
