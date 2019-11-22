60504e1e38524700f176495e847c428179b267cd
SeaWar741 <juanca741@gmail.com>
2019-11-22 02:31:20 -0600

    added progress on finding function

--- 1er_Semestre/Modelacion_computacional_leyes_de_conservacion/entregable1.m
+++ 1er_Semestre/Modelacion_computacional_leyes_de_conservacion/entregable1.m
@@ -7,11 +7,16 @@ y1=500;
 y2=200;
 
 %x3=967;
-x3=1000;
-y3=-500;
+x3=725;
+y3=-1547.723;
 %x4=1933.33333;
-x4=2000;
-y4=1000;
+x4=2166.445;
+y4=549.482;
+
+%Buenos:
+%x3=1000;y3=-500;x4=2000;y4=1000; ---> 204.151062269073
+%x3=1000;y3=0;x4=2000;y4=1500;--> 199.855206068844
+%x3=1000;y3=-50;x4=2050;y4=1500;--> 197.690634031603
 
 eq1= a3*x1^3+a2*x1^2+a1*x1+a0==y1;
 eq2= a3*x2^3+a2*x2^2+a1*x2+a0==y2;
@@ -37,14 +42,24 @@ fprintf("Longitud de arco: %0.4f\n",lg);
 %%
 %%Radio de curvatura
 rval= zeros(1,2800);
+min =250;
 for i=100:1:2800
     p = ((fxl)^(2/3)) / (diff(fxd));
     psub = subs(p,x,i);
     rval(1,i) = psub;
-    if(psub<=270&&psub>=0)
-        fprintf("Radio: %.6f\n",psub);
+    if(psub<=210&&psub>=0)
+        %fprintf("Radio: %.6f\n",psub);
+        if(psub<=min)
+            min = psub;
+        end
+    end
+    
+    if(i==100)
+        min = psub;
     end
     %fprintf("El radio de curvatura es: \n");
 end
+
+fprintf("El valor minimo del radio de curvatura es de: %.12f\n",min);
 
 %disp(rval);
