6dc93a257b2b8cdd9d82a6deb8a5a7e8a97af695
SeaWar741 <juanca741@gmail.com>
2019-11-24 18:35:09 -0600

    adding merge of codes

--- 1er_Semestre/Modelacion_computacional_leyes_de_conservacion/tangente.m
+++ 1er_Semestre/Modelacion_computacional_leyes_de_conservacion/tangente.m
@@ -1,4 +1,4 @@
-y=-0.000001044*x^3+0.004317333*x^2+-4.146117609*x+872.482131254
+y=-0.000001044*x^3+0.004317333*x^2+-4.146117609*x+872.482131254;
 
 puntox= 541;
 
@@ -8,7 +8,11 @@ puntoy=subs(y,x,puntox);
 fprintf("Pendiente: %.9f\n",m);
 fprintf("Puntos: (%.9f,%.9f)\n",puntox,puntoy);
 y2=m*x-m*puntox+puntoy;
+fprintf("La funcion es: y=");
 disp(y2);
 
 f1 = sqrt( (x-puntox)^2 + (y-puntoy) )==20;
 
+x2= puntox+20*sqrt(1/(1+m^2));
+y2= puntoy+m*20*sqrt(1/(1+m^2));
+fprintf("Puntos: (%.9f,%.9f)\n",x2,y2);
