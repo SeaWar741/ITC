0eba4009c84b4553a1f75c72a0b2c63d14ea0b45
SeaWar741 <juanca741@gmail.com>
2019-12-03 21:30:09 -0600

    starting app

--- 1er_Semestre/Modelacion_computacional_leyes_de_conservacion/testing2.m
+++ 1er_Semestre/Modelacion_computacional_leyes_de_conservacion/testing2.m
@@ -2,6 +2,9 @@ x = 100:.01:2800;
 y = -0.000002249*x.^3+0.010770716*x.^2-13.061982421*x+1700.740063337;
 px = 100;
 py = 500;
+
+
+% => 2 diferent boxes!
 for i=1:100
     %figure(100); %This is so it will replot over the previous figure, and not make a new one.
     ph = plot(x,y,px, py,'o');
