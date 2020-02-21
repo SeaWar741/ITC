87fea8ce0c663893fe5d287d78c39b89defd98e4
SeaWar741 <juanca741@gmail.com>
2020-02-20 23:54:35 -0600

    changing formula

--- 2do_Semestre/Matematicas_discretas/model.py
+++ 2do_Semestre/Matematicas_discretas/model.py
@@ -1,14 +1,15 @@
 from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
-fig = plt.figure()
-ax = plt.axes(projection='3d')
+
+a = 0.1
+b = 0.1
 
 def f(x, y):
-    return np.sin(np.sqrt(x ** 2 + y ** 2))
+    return (x+y)
 
-x = np.linspace(-6, 6, 30)
-y = np.linspace(-6, 6, 30)
+x = np.linspace(-10, 10, 15)
+y = np.linspace(-10, 10, 35)
 
 X, Y = np.meshgrid(x, y)
 Z = f(X, Y)
@@ -19,3 +20,5 @@ ax.contour3D(X, Y, Z, 50, cmap='binary')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z');
+
+plt.show()
