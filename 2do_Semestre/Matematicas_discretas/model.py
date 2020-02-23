0bffedaa4fdef22b1c685f79a2a39a6729a9001a
SeaWar741 <juanca741@gmail.com>
2020-02-22 18:12:48 -0600

    added functions for 3d figure made out of vertical walls

--- 2do_Semestre/Matematicas_discretas/model.py
+++ 2do_Semestre/Matematicas_discretas/model.py
@@ -8,21 +8,35 @@ x=np.arange(16)
 y=np.arange(16)
 z=np.arange(16)
 
-def f(x, y):
-    return (0.5*x**4+0.5*y**4)
+#Funciones respecto a y
+def f1(x, y):
+    return (2*x+5)
+def f2(x,y):
+    return (-2*x+5)
+def f3(x,y):
+    return (2*x-5)
+def f4(x,y):
+    return (-2*x-5)
 
-x = np.linspace(-4000, 4000)
-y = np.linspace(-4000, 4000)
+x = np.linspace(-4, 4)
+y = np.linspace(-4, 4)
 
 X, Y = np.meshgrid(x, y)
-Z = f(X, Y)
-
+Z1 = f1(X,Y)
+Z2 = f2(X,Y)
+Z3 = f3(X,Y)
+Z4 = f4(X,Y)
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.contour3D(X, Y, Z, 80, cmap='binary')
+
+ax.contour3D(X, Y, Z1, 80, cmap='binary')
+ax.contour3D(X, Y, Z2, 80, cmap='binary')
+ax.contour3D(X, Y, Z3, 80, cmap='binary')
+ax.contour3D(X, Y, Z4, 80, cmap='binary')
 
 ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([0.5, 0.5, 1, 1]))
+
 
 ax.set_xlabel('x')
 ax.set_ylabel('y')
