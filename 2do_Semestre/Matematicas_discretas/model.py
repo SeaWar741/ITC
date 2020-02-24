b00ebb1fcc2250fc3f17d516ed722ea1223f0835
SeaWar741 <juanca741@gmail.com>
2020-02-23 23:34:16 -0600

    added new plot style

--- 2do_Semestre/Matematicas_discretas/model.py
+++ 2do_Semestre/Matematicas_discretas/model.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 #0.5*x^4+0.5y^4
-
+'''
 x=np.arange(16)
 y=np.arange(16)
 z=np.arange(16)
@@ -42,4 +42,32 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z');
 
+plt.show()
+'''
+yy, zz = np.meshgrid(range(4), range(4))
+xx = (yy-5)/2
+
+yy1, zz1 = np.meshgrid(range(4), range(4))
+xx1 = (yy1-5)/(-2)
+
+yy2, zz2 = np.meshgrid(np.linspace(-4, 0, 4) , np.linspace(0, 4, 4))
+xx2 = (yy2+5)/2
+
+print(zz2)
+
+yy3, zz3 = np.meshgrid(np.linspace(-4, 0, 4) , np.linspace(0, 4, 4))
+xx3 = (yy3+5)/(-2)
+
+
+yy4,xx4= np.meshgrid(np.linspace(-12, 12, 25), np.linspace(-5, 5, 11) )
+zz4 = (0.5*(xx4**4))+(0.8*(yy4**4))
+
+print(yy4)
+
+ax = plt.subplot(projection='3d')
+ax.plot_surface(xx, yy, zz)
+ax.plot_surface(xx1, yy1, zz1)
+ax.plot_surface(xx2, yy2, zz2)
+ax.plot_surface(xx3, yy3, zz3)
+ax.plot_surface(xx4, yy4, zz4)
 plt.show()
