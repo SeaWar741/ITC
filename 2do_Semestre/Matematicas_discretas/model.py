27ec1fb7ba688c5c1d8a93114641bc349ee7d563
SeaWar741 <juanca741@gmail.com>
2020-02-26 02:14:15 -0600

    understood how to use mayavi, applied to join hexagonal faces

--- 2do_Semestre/Matematicas_discretas/model.py
+++ 2do_Semestre/Matematicas_discretas/model.py
@@ -1,87 +1,13 @@
+'''
 from mpl_toolkits.mplot3d import Axes3D
 import mpl_toolkits.mplot3d.art3d as art3d
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Circle
 from tkinter import *
+'''
 #0.5*x^4+0.5y^4
-'''
-x=np.arange(16)
-y=np.arange(16)
-z=np.arange(16)
 
-#Funciones respecto a y
-def f1(x, y):
-    return (2*x+5)
-def f2(x,y):
-    return (-2*x+5)
-def f3(x,y):
-    return (2*x-5)
-def f4(x,y):
-    return (-2*x-5)
-
-x = np.linspace(-4, 4)
-y = np.linspace(-4, 4)
-
-X, Y = np.meshgrid(x, y)
-Z1 = f1(X,Y)
-Z2 = f2(X,Y)
-Z3 = f3(X,Y)
-Z4 = f4(X,Y)
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-
-ax.contour3D(X, Y, Z1, 80, cmap='binary')
-ax.contour3D(X, Y, Z2, 80, cmap='binary')
-ax.contour3D(X, Y, Z3, 80, cmap='binary')
-ax.contour3D(X, Y, Z4, 80, cmap='binary')
-
-ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([0.5, 0.5, 1, 1]))
-
-
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z');
-
-plt.show()
-'''
-
-#hexagonal
-'''
-yy, zz = np.meshgrid(range(4), range(4))
-xx = (yy-5)/2
-
-yy1, zz1 = np.meshgrid(range(4), range(4))
-xx1 = (yy1-5)/(-2)
-
-print(zz1)
-
-yy2, zz2 = np.meshgrid(np.linspace(-4, 0, 4) , np.linspace(0, 4, 4))
-xx2 = (yy2+5)/2
-
-print(zz2)
-
-yy3, zz3 = np.meshgrid(np.linspace(-4, 0, 4) , np.linspace(0, 4, 4))
-xx3 = (yy3+5)/(-2)
-
-
-xx4,zz4 = np.meshgrid(range(4),range(4))
-yy4 = xx4*0
-
-#yy4,xx4= np.meshgrid(np.linspace(-12, 12, 25), np.linspace(-5, 5, 11) )
-#zz4 = (0.5*(xx4**4))+(0.8*(yy4**4))
-
-#print(yy4)
-
-ax = plt.subplot(projection='3d')
-ax.plot_surface(xx, yy, zz)
-ax.plot_surface(xx1, yy1, zz1)
-ax.plot_surface(xx2, yy2, zz2)
-ax.plot_surface(xx3, yy3, zz3)
-ax.plot_surface(xx4, yy4, zz4)
-#ax.plot_surface(xx4, yy4, zz4)
-plt.show()
 '''
 
 fig = plt.figure()
@@ -110,3 +36,50 @@ ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("Z")
 plt.show()
+'''
+# Create the data.
+
+
+
+import numpy as np
+from mayavi import mlab
+
+x1,y1,z1 = (-1.5,2,0)
+x2,y2,z2 = (1.5,2,0)
+x3,y3,z3 = (-2.5,0,0)
+x4,y4,z4 = (-1.5,-2,0)
+x5,y5,z5 = (1.5,-2,0)
+x6,y6,z6 = (2.5,0,0)
+
+x7,y7,z7 = (-1.5,2,12)
+x8,y8,z8 = (1.5,2,12)
+x9,y9,z9 = (-2.5,0,12)
+x10,y10,z10 = (-1.5,-2,12)
+x11,y11,z11 = (1.5,-2,12)
+x12,y12,z12 = (2.5,0,12)
+
+box_piunts = np.array([x1,y1,z1],[x2,y2,z2],[x3,y3,z3],[x4,y4,z4],[x5,y5,z5],[x6,y6,z6],[x7,y7,z7,],[x8,y8,z8],[x9,y9,z9],[x10,y10,z10],[x11,y11,z11],[x12,y12,z12])
+mlab.points3d(box_points[:, 0], box_points[:, 1], box_points[:, 2],
+                     mode="axes", color=(1, 0, 0))
+
+mlab.mesh([[x1, x2],[x3, x4],[x5, x5],[x6, x7],[x8, x8],[x9, x9],[x10, x10],
+[x11, x11],[x12, x12]],  # | => x coordinate
+
+                 [[y1, y2],
+                  [y3, y4]],  # | => y coordinate
+
+                 [[z1, z2],
+                  [z3, z4]],  # | => z coordinate
+
+                 color=(0, 0, 0))  # black
+
+
+mlab.clf()
+phi, theta = np.mgrid[0:np.pi:11j, 0:2*np.pi:11j]
+x = np.sin(phi) * np.cos(theta)
+y = np.sin(phi) * np.sin(theta)
+z = np.cos(phi)
+mlab.mesh(x, y, z)
+mlab.mesh(x, y, z, representation='wireframe', color=(0, 0, 0))
+
+mlab.show()
