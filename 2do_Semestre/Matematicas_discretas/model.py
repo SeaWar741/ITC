19b6e11f8c6fe1fe7bce0a96fbc1e5cc36c0eb20
SeaWar741 <juanca741@gmail.com>
2020-02-26 02:25:01 -0600

    added all points, todo: connect proper points to create hexagon

--- 2do_Semestre/Matematicas_discretas/model.py
+++ 2do_Semestre/Matematicas_discretas/model.py
@@ -39,8 +39,6 @@ plt.show()
 '''
 # Create the data.
 
-
-
 import numpy as np
 from mayavi import mlab
 
@@ -58,28 +56,43 @@ x10,y10,z10 = (-1.5,-2,12)
 x11,y11,z11 = (1.5,-2,12)
 x12,y12,z12 = (2.5,0,12)
 
-box_piunts = np.array([x1,y1,z1],[x2,y2,z2],[x3,y3,z3],[x4,y4,z4],[x5,y5,z5],[x6,y6,z6],[x7,y7,z7,],[x8,y8,z8],[x9,y9,z9],[x10,y10,z10],[x11,y11,z11],[x12,y12,z12])
+box_points = np.array([[x1,y1,z1],[x2,y2,z2],[x3,y3,z3],[x4,y4,z4],[x5,y5,z5],[x6,y6,z6],[x7,y7,z7,],[x8,y8,z8],[x9,y9,z9],[x10,y10,z10],[x11,y11,z11],[x12,y12,z12]])
 mlab.points3d(box_points[:, 0], box_points[:, 1], box_points[:, 2],
                      mode="axes", color=(1, 0, 0))
 
-mlab.mesh([[x1, x2],[x3, x4],[x5, x5],[x6, x7],[x8, x8],[x9, x9],[x10, x10],
-[x11, x11],[x12, x12]],  # | => x coordinate
+mlab.mesh([[x1, x2],[x3, x4],[x5, x6],[x7, x8],[x9, x10],[x11, x12]],  # | => x coordinate
 
-                 [[y1, y2],
-                  [y3, y4]],  # | => y coordinate
+                [[y1, y2],[y3, y4],[y5, y6],[y7, y8],[y9, y10],[y11, y12]],  # | => y coordinate
 
-                 [[z1, z2],
-                  [z3, z4]],  # | => z coordinate
+                 [[z1, z2],[z3, z4],[z5, z6],[z7, z8],[z9, z10],[z11, z12]],  # | => z coordinate
 
                  color=(0, 0, 0))  # black
 
+mlab.mesh([[x5, x6], [x7, x8]],
+                 [[y5, y6], [y7, y8]],
+                 [[z5, z6], [z7, z8]],
+                 color=(1, 0, 0))  # red
 
-mlab.clf()
-phi, theta = np.mgrid[0:np.pi:11j, 0:2*np.pi:11j]
-x = np.sin(phi) * np.cos(theta)
-y = np.sin(phi) * np.sin(theta)
-z = np.cos(phi)
-mlab.mesh(x, y, z)
-mlab.mesh(x, y, z, representation='wireframe', color=(0, 0, 0))
+mlab.mesh([[x1, x3], [x5, x7]],
+                 [[y1, y3], [y5, y7]],
+                 [[z1, z3], [z5, z7]],
+                 color=(0, 0, 1))  # blue
+
+mlab.mesh([[x1, x2], [x5, x6]],
+                 [[y1, y2], [y5, y6]],
+                 [[z1, z2], [z5, z6]],
+                 color=(1, 1, 0))  # yellow
+
+mlab.mesh([[x2, x4], [x6, x8]],
+                 [[y2, y4], [y6, y8]],
+                 [[z2, z4], [z6, z8]],
+                 color=(1, 1, 1))  # white
+
+mlab.mesh([[x3, x4], [x7, x8]],
+                 [[y3, y4], [y7, y8]],
+                 [[z3, z4], [z7, z8]],
+                 color=(1, 0, 1))  # pink
+
+
 
 mlab.show()
