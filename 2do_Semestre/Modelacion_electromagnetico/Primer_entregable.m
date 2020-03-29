2aac43982806c8fe042aa0020cf5f5c636d1451c
SeaWar741 <juanca741@gmail.com>
2020-03-29 17:13:50 -0600

    added field

--- 2do_Semestre/Modelacion_electromagnetico/Primer_entregable.m
+++ 2do_Semestre/Modelacion_electromagnetico/Primer_entregable.m
@@ -1,4 +1,5 @@
-puntos = 5;%numero de puntos en la malla
+clear;
+puntos = 25;%numero de puntos en la malla
 cargas=[2,-2];%dos cargas de diferentes signos
 radio = 0.2;%radio de las cargas
 
@@ -7,7 +8,7 @@ xcargas=[-0.5,0.5];
 ycargas=[0,0];
 
 
-espsilon = 8.854e-12;%Escribir el valor de epsilon
+epsilon = 8.854e-12;%Escribir el valor de epsilon
 %es un valor para hacer la ecuacion del campo electrico
 k = 1/(4 * pi * epsilon);
 
@@ -22,7 +23,7 @@ miny= -1.5;
 campoex = zeros(puntos);
 campoey = zeros(puntos);
 
-
+%Crear 2 vectores
      %minimo y maximo para numero de divisiones
 x =linspace(minx,maxx,puntos);%va de menos -1.5 a 1.5 dividido en npuntos
 y =linspace(miny,maxy,puntos);%estos son vectores
@@ -40,11 +41,18 @@ for i=1:2
     r = sqrt(dx.^2+dy.^2);%distancias totales/ magnitud
     r3 = r.^3;%distnacias entre donde esta la carga y los elementos que rodean la carga
     
+    %aqui es por componentes
     campoex=campoex+k.*cargas(i).*dx./r3;
     campoey=campoey+k.*cargas(i).*dy./r3;
     
-    
 end
 
+campoe_total = sqrt(campoex.^2+campoey.^2);
+disp(campoe_total);
 
-
+%graficar
+quiver(xpuntosmalla,ypuntosmalla,campoex./campoe_total,campoey./campoe_total);%se aplica normalizacion
+hold on
+%usar rectangulo, usando la posicion con radio
+%axis para el tamaño de la grafica}
+%para el nombre de la carga, poner sobre el rectangulo, usar text
