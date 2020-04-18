28d4acc975fa449c1a1677b86e7fd802cb271cc0
SeaWar741 <juanca741@gmail.com>
2020-04-18 12:47:59 -0500

    finished tarea2

--- 2do_Semestre/Biologia_computacional/temp_2.r
+++ 2do_Semestre/Biologia_computacional/temp_2.r
@@ -1,54 +1,12 @@
-#EJERCICIO 1
-file <- 'PACIENTES_DE_NUEVO_INGRESO.csv'
-dataa <- read.csv(file)
-
-# <-----b)----->
-tumores = length(unique(dataa$DESCRIPCION.DIAGNOSTICO))
-cat("\n\n -----------------------------------------------------------------------\n") 
-cat("|Abraham Cepeda,Juan Carlos Garfias,Lucas Eduardo Idígoras y Vanina Sada|\n")
-cat(" -----------------------------------------------------------------------\n") 
-cat("\n\n<----Cantidad de tumores diferentes---->\n\n")
-cat("CANTIDAD DE TUMORES:\t",tumores) 
-
-estados = sort(table(dataa$ESTADO))
-diezEstados = estados[(length(estados)-9):length(estados)] 
-cat("\n\n<----Diez estados con mayor número de casos---->\n\n")
-print(diezEstados) 
-
-municipios = sort(table(dataa$MUNICIPIO))
-diezMunicipios = municipios[(length(municipios)-9):length(municipios)]    
-cat("\n\n<----Diez municipios con mayor número de casos---->\n\n")
-print(diezMunicipios) 
-
-# <-----c)----->
-tumorColonRecto = length(dataa$DESCRIPCION.DIAGNOSTICO[dataa$DESCRIPCION.DIAGNOSTICO == "TUMOR MALIGNO DEL COLON" | dataa$DESCRIPCION.DIAGNOSTICO == "TUMOR MALIGNO DEL RECTO"])
-cat("\n\n<----Número de casos de tumor de colon o recto---->\n\n")
-cat("TUMOR COLOR/RECTO:",tumorColonRecto)
-
-tumorColonJovenes = length(dataa$DESCRIPCION.DIAGNOSTICO[dataa$EDAD <= 29 & dataa$DESCRIPCION.DIAGNOSTICO == "TUMOR MALIGNO DEL COLON"])
-cat("\n\n<----Número de casos de tumor de colon o recto en jóvenes (menor a 29 años)---->\n\n")
-cat("NUMERO EN JOVENES:",tumorColonJovenes)
-
-tumorColonJovenesA = length(dataa$DESCRIPCION.DIAGNOSTICO[dataa$EDAD < 50 & dataa$DESCRIPCION.DIAGNOSTICO == "TUMOR MALIGNO DEL COLON"])
-cat("\n\n<----Número de casos de tumor de colon o recto en jóvenes adultos (menor a 50 años)---->\n\n")
-cat("NUMERO EN JOVENES:",tumorColonJovenesA)
-
-tumorCRE = rev(sort(table(dataa$ESTADO[dataa$DESCRIPCION.DIAGNOSTICO == "TUMOR MALIGNO DEL COLON" | dataa$DESCRIPCION.DIAGNOSTICO == "TUMOR MALIGNO DEL RECTO"])))
-cat("\n\n<----Número de casos de tumor de colon o recto por estado---->\n\n")
-print(tumorCRE[1:10])
-
-tumorCRM = rev(sort(table(dataa$MUNICIPIO[dataa$DESCRIPCION.DIAGNOSTICO == "TUMOR MALIGNO DEL COLON" | dataa$DESCRIPCION.DIAGNOSTICO == "TUMOR MALIGNO DEL RECTO"])))
-cat("\n\n<----Número de casos de tumor de colon o recto por municipio---->\n\n")
-print(tumorCRM[1:10])
-
-#EJERCICIO 2
 #a) Compara todas las muestras de cáncer contra todos los normales.
 
 #b) Compara las muestras de cada cáncer contra su respectivo normal. En el caso de leucemia o linfoma, usa las muestras normales que dicen “Germinal” y “peripheral blood”; para glioblastoma usa “whole brain” y para meduloblastoma usa “cerebellum”. Describe también por qué se deben hacer así estas comparaciones.
 
 #c) Compara si los genes con medias más diferentes en las comparaciones de cánceres individuales son similares al de la comparación de todas las muestras. Comenta en especial para el caso de cáncer colorectal.
 
-load("Multi_Cancer_Data.RData")
+
+
+load("Multi_Cancer_Data.RData", verbose = TRUE)
 df <- data.frame(multi_cancer_data)
 
 #print(colnames(dataFile))
@@ -58,9 +16,10 @@ normal <- df[,191:280]
 #write.csv(normal,"normal.csv", row.names = FALSE)
 
 #obtiene promedios
-meandt <-as.data.frame(t(apply((tumors),MARGIN=2,FUN=mean))) 
+meandt <-as.data.frame(t(apply((tumors),MARGIN=2,FUN=mean)))
 meann <-as.data.frame(t(apply((normal),MARGIN=2,FUN=mean)))
 #print(length(meann))
+
 
 #a)
 totalm_dt= 0
@@ -75,10 +34,8 @@ for(j in 1:90){
 totalm_dt <- totalm_dt/190
 totalm_dn <- totalm_dn/90
 diff_avg <- abs(totalm_dt-totalm_dn)
-cat("-=COMPARACION GENERAL TUMORES CONTRA NORMALES=-\n\n")
-cat("\nPROMEDIO TUMORES:",totalm_dt,"| PROMEDIO NOMRALES:",totalm_dn,"| DIFERENCIA:",diff_avg,"\n", sep="\t")
 
-#b)
+cat("\nPROMEDIO TUMORES:",totalm_dt,"| PROMEDIO NOMRALES:",totalm_dn,"| DIFERENCIA:",diff_avg,"\n", sep="\t")
 
 #test
 
@@ -198,4 +155,3 @@ print(table_comparison)
 type_list[18] = "Media total"
 vector_f <- (c(vector_dif,diff_avg))
 barplot(vector_f, main="Diferencias con promedio", names.arg=type_list, cex.names=0.4)
-
