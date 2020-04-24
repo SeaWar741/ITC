83265b15db9d30dcfe02b56399c030214fb3f6c0
SeaWar741 <juanca741@gmail.com>
2020-04-23 20:23:06 -0500

    added files obtencion de raices finished

--- 2do_Semestre/Modelacion_electromagnetico/Raiz_polinomios/Raicesp.m
+++ 2do_Semestre/Modelacion_electromagnetico/Raiz_polinomios/Raicesp.m
@@ -19,5 +19,4 @@ function r = Raicesp(a)
             a = nuevaa;
         end    
     end
-    
 end
--- 2do_Semestre/Modelacion_electromagnetico/Raiz_polinomios/raiz_polinomio.m
+++ 2do_Semestre/Modelacion_electromagnetico/Raiz_polinomios/raiz_polinomio.m
@@ -1,6 +1,8 @@
 %Juan Carlos Garfias Tovar
 clc;clear all;close;
 
+%funcion1
+figure(1)
 p = @(x) x.^3-6.*x.^2+11.*x-6;
 x = -1:0.1:5;
 plot(x,p(x));
@@ -9,9 +11,64 @@ hold on
 a= [1 -6 11 -6];
 
 r= Raicesp(a);
-disp('Las raices son:')
+disp('1)Las raices son:')
 disp(r)
 %evaluar raices
 a=p(r);
 %graficar raices
 plot(r,a,'or');
+
+%-------------------------------------------------------------------------
+%funcion2
+figure(2)
+p=@(x)x.^3-x.^2-4.*x+4;
+x = -5:0.1:5;
+plot(x,p(x));
+grid on
+hold on
+a= [1 -1 -4 4];
+
+r= Raicesp(a);
+disp('2)Las raices son:')
+disp(r)
+%evaluar raices
+a=p(r);
+%graficar raices
+plot(r,a,'or');
+
+%-------------------------------------------------------------------------
+%funcion3
+figure(3)
+p=@(x)-x.^4+10.*x.^2-9;
+x = -4:0.1:4;
+plot(x,p(x));
+grid on
+hold on
+a= [-1 0 10 0 -9];
+
+r= Raicesp(a);
+disp('3)Las raices son:')
+disp(r)
+%evaluar raices
+a=p(r);
+%graficar raices
+plot(r,a,'or');
+
+%-------------------------------------------------------------------------
+%funcion4
+figure(4)
+p=@(x)2.*x.^3+x.^2-13.*x+6;
+x = -4:0.1:4;
+plot(x,p(x));
+grid on
+hold on
+a= [2 1 -13 6];
+
+r= Raicesp(a);
+disp('4)Las raices son:')
+disp(r)
+%evaluar raices
+a=p(r);
+%graficar raices
+plot(r,a,'or');
+
