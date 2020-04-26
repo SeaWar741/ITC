172a4e111f3d282ebc9597aef75460ae6e4c27d6
SeaWar741 <juanca741@gmail.com>
2020-04-25 23:51:28 -0500

    added changes to tarea4

--- 2do_Semestre/Biologia_computacional/Tarea4/Tarea_4.r
+++ 2do_Semestre/Biologia_computacional/Tarea4/Tarea_4.r
@@ -3,3 +3,75 @@
 #Genes diferencialmente expresados usando cada método.
 #Saber si las listas de genes son comparables. Para esto puedes hacer una gráfica (scatter) comparando los ranqueos de los genes (un método en el eje X y otro en Y).
 #Ver cuáles vías de señalización están alteradas de acuerdo a cada método y compararlas.
+
+library(gtools)
+
+load("TCGA_COADREAD_comp_data.RData", verbose = TRUE)
+df <- data.frame(tcga_coadread)
+type <- data.frame(tcga_coadread_class)
+type <- as.vector(type[,1])
+yindex <- vector()
+oindex <- vector()
+
+nyoung<-0
+nold<-0
+
+counter1 =1
+counter2 =1
+for(i in 1:length(type)){
+  if(type[i]=="Young"){
+    nyoung<-nyoung+1
+    yindex[counter1]=i
+    counter1=counter1+1
+  }else{
+    nold<-nold+1
+    oindex[counter2]=i
+    counter2=counter2+1
+  }
+}
+
+
+#T Test
+young <- df[,yindex]
+old <- df[,oindex]
+
+pvalues <-data.frame(matrix(, nrow=length(df[,1]), ncol=1))
+
+for(i in 1:nrow(df)){
+  pvalues[i,1]<-(t.test(df[i,yindex],df[i,oindex])$p.value)
+}
+colnames(pvalues)<-c("Pvalues")
+write.csv(pvalues,"pvalues.csv", row.names = TRUE)
+
+#Getting mean graphs
+type <- data.frame(tcga_coadread_class)
+dataNew <- data.frame(matrix(0, nrow = length(df[[1]]), ncol = 2))
+countYoung <- 0
+countOld <- 0
+for (i in 1:length(df)) {
+  if(type[[1]][i] == "Young"){
+    dataNew[[1]] = dataNew[[1]] +  df[[i]]
+    countYoung <- countYoung + 1
+  } else{
+    dataNew[[2]] = dataNew[[2]] +  df[[i]]
+    countOld <- countOld + 1
+  }
+} 
+for(i in 1:length(dataNew[[1]])){
+  dataNew[[1]][i] = dataNew[[1]][i]/countYoung
+  dataNew[[2]][i] = dataNew[[2]][i]/countOld
+}
+#colnames(dataNew)<-colnames(df)
+write.csv(dataNew,"Averages_Comparison.csv", row.names = TRUE)
+
+#Plotting
+par(mfrow=c(2,2))
+hist(dataNew$X1)
+hist(dataNew$X2)
+boxplot(young)
+boxplot(old)
+
+hist(pvalues$Pvalues)
+barplot(pvalues$Pvalues)
+
+plot(dataNew$X2,c(1:length(dataNew$X2)))
