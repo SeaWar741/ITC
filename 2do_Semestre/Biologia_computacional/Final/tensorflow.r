9fa65abaf820ceed5649a1055ed4df769abe688a
SeaWar741 <juanca741@gmail.com>
2020-05-03 23:27:50 -0500

    added final changes to project ingenieriabiologia

--- 2do_Semestre/Biologia_computacional/Final/tensorflow.r
+++ 2do_Semestre/Biologia_computacional/Final/tensorflow.r
@@ -1,6 +1,5 @@
 library(tidyverse)
 library(keras)
-library(ggplot2)
 library(dplyr)
 library(RColorBrewer)
 
@@ -30,18 +29,52 @@ for(i in 1:length(type)){
     counter2=counter2+1
   }
 }
-#Divididos
+#Divididos y transpuestos
 young <- t(df[,yindex])
 old <- t(df[,oindex])
+df <- t(df)
 
 
-#Clustering Young
+#Clustering all
 set.seed(2345)
-kmeans.ani(young, 3)
-young_cluster <- kmeans(young, 3)
+#par(mfrow=c(2,7))
+kmeans.ani(df, 2)
+df_cluster <- kmeans(df, 2)
 
 kmean_withinss <- function(k) {
-    cluster <- kmeans(young, k)
+    cluster <- kmeans(df, k)
+    return (cluster$tot.withinss)
+}
+print("Total within clusters sum of squares")
+print(kmean_withinss(2))
+
+#start of algorithm
+# Set maximum cluster 
+max_k <-20 
+# Run algorithm over a range of k 
+wss <- sapply(2:max_k, kmean_withinss)
+# Create a data frame to plot the graph
+elbow <-data.frame(2:max_k, wss)
+# Plot the graph with gglop
+ggplot(elbow, aes(x = X2.max_k, y = wss)) +
+    geom_point() +
+    geom_line() +
+    scale_x_continuous(breaks = seq(1, 20, by = 1))
+
+#print("Cluster size")
+#print(df$size)
+
+Sys.sleep(10)
+
+#----------------------------------------------------------------------
+#Clustering Young
+set.seed(2345)
+#par(mfrow=c(2,7))
+kmeans.ani(df, 2)
+df_cluster <- kmeans(df, 2)
+
+kmean_withinss <- function(k) {
+    cluster <- kmeans(df, k)
     return (cluster$tot.withinss)
 }
 print("Total within clusters sum of squares")
@@ -66,7 +99,10 @@ print(young_cluster$size)
 Sys.sleep(10)
 
 #---------------------------------------------------------------
+cat("\n")
+x11()   
 set.seed(2345)
+#par(mfrow=c(2,7))
 kmeans.ani(old, 3)
 old_cluster <- kmeans(old, 3)
 
