0f0b8c3e66feffe455b377aeb09a16416cacc905
SeaWar741 <juanca741@gmail.com>
2020-08-10 18:25:14 -0500

    minor fixes, added documentation to hw

--- 3er_Semestre/EstructuraDeDatosYAlgoritmos/Templates/EjercicioTemplate.cpp
+++ 3er_Semestre/EstructuraDeDatosYAlgoritmos/Templates/EjercicioTemplate.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//Funcion la cual imprime el menu de seleccion de tipo de dato
 void selectionMenu(){
     cout<<"Selecciona el tipo de dato a almacenar: "<<endl;
     cout<<"1) int"<<endl;
@@ -13,13 +14,16 @@ void selectionMenu(){
     cout<<"5) bool"<<endl;
 }
 
+
+//Se llama a template para establecer la variable T
+//Esta funcion se encarga del funcionamiento general del programa
 template <class T>
 void menu(Lista<T> arry){
     
-    T insertion;
-    int selection,index;
-    bool continues = true;
-    while(continues){
+    T insertion;//variable para insertar datos
+    int selection,index;//variable para los indices
+    bool continues = true;//variable para cerrar programa
+    while(continues){ //Mientras que no se seleccione salir, el programa continuara en loop
         cout<<"1) Insert"<<endl;
         cout<<"2) Erase"<<endl;
         cout<<"3) Get Data"<<endl;
@@ -27,41 +31,41 @@ void menu(Lista<T> arry){
         cout<<"5) Print"<<endl;
         cout<<"6) Exit"<<endl<<endl;
         cout<<"Ingresa tu seleccion: ";cin>>selection;cout<<endl;
-        switch (selection){
+        switch (selection){ //se hace un switch a partir de la seleccion de usuario
             case 1:
-                cout<<"Ingresar elemento: ";cin>>insertion;cout<<endl;
-                try{
+                try{ //se hace un try catch para insertar datos
+                    cout<<"Ingresar elemento: ";cin>>insertion;cout<<endl;
                     arry.insert(insertion);
                 }catch(runtime_error& e){
-                    cout<<e.what()<<endl;
+                    cout<<e.what()<<endl; //se imprime el error dado
                 }
                 break;
             case 2:
                 try{
-                    cout<<arry.erase()<<endl;
+                    cout<<arry.erase()<<endl; //se hace try catch
                 }catch(runtime_error& e){
-                    cout<<e.what()<<endl;
+                    cout<<e.what()<<endl; //se muestra error en caso de fallo
                 }
                 break;
             case 3:
-                cout<<"Ingresar indice: ";cin>>index;cout<<endl;
-                try{
+                try{//try catch para el indice a obtener
+                    cout<<"Ingresar indice: ";cin>>index;cout<<endl;
                     cout<<arry.getData(index)<<endl;
                 }catch(runtime_error& e){
                     cout<<e.what()<<endl;
                 }
                 break;
             case 4:
-                cout<<arry.getSize()<<endl;
+                cout<<arry.getSize()<<endl; //se imprime el tamaño
                 break;
             case 5:
-                arry.print();
+                arry.print(); //se imprime el array
                 break;
             case 6:
-                continues = false;
+                continues = false; //se cancela el loop y termina programa
                 break;
             default:
-                cout<<"Opcion no valida"<<endl;
+                cout<<"Opcion no valida"<<endl;//caso default/ no valido
                 break;
         }
         cout<<endl;
@@ -72,18 +76,18 @@ void menu(Lista<T> arry){
 
 int main(){
     
-    int selection;
-    Lista<int> lista1; 
-    Lista<double> lista2; 
-    Lista<string> lista3; 
-    Lista<char> lista4; 
-    Lista<bool> lista5;
+    int selection; //seleccion base 
+    Lista<int> lista1; //lista de ints
+    Lista<double> lista2; //lista de doubles
+    Lista<string> lista3; //lista de strings
+    Lista<char> lista4; //lista de chars
+    Lista<bool> lista5; //lista de bools
 
-    selectionMenu();
+    selectionMenu(); //se llama al menu base
 
     cout<<"Ingresar seleccion: ";cin>>selection;cout<<endl;
-    try{
-         switch (selection){
+    try{//try catch general para el programa
+         switch (selection){ //switch para hacer operaciones a partir de lista
             case 1:
                 menu(lista1);
                 break;
@@ -105,7 +109,7 @@ int main(){
                 break;
         } 
     }
-    catch(const std::exception& e){
+    catch(const std::exception& e){ //se imprime el error y cierra programa
         cout<<"Error de ejecución, verificar inputs y volver a iniciar el programa"<<endl;
     }
 }
--- 3er_Semestre/EstructuraDeDatosYAlgoritmos/Templates/Lista.h
+++ 3er_Semestre/EstructuraDeDatosYAlgoritmos/Templates/Lista.h
@@ -6,75 +6,87 @@ using namespace std;
 
 #ifndef Lista_h
 #define Lista_h
-
+ 
+//se llama a template
 template <class T>
 class Lista{
 private:
-    static const int MAX = 100;
-    vector <T> data;
-    int size;
+    static const int MAX = 100; //constante MAX
+    vector <T> data;//vector de dato T (variable en tipo)
+    int size;//tamaño del array/vector
 public:
-    Lista();
-    void insert(T dataI);
-    T erase();
-    T getData(int index);
-    int getSize();
-    void print();
+    Lista(); //constructor
+    void insert(T dataI); //insert con dato T
+    T erase(); //borrar regresando dato T
+    T getData(int index); //obtener con dato T
+    int getSize(); //regresar tamaño
+    void print(); //imprimir lista
 };
 
+//se llama a template
 template <class T>
-Lista<T>::Lista(){
+Lista<T>::Lista(){ //constructor
     size = 0;
 }
 
+//se llama a template
 template <class T>
 void Lista<T>::insert(T dataI){
-    if(size != MAX){
+    if(size != MAX){ //si el tamaño no es el maximo se mete dato
         this->data.push_back(dataI);
+        size++;//incrementa el size
     }
-    else{
+    else{//en caso de que falle (out of bounds) sale error
         throw runtime_error("LISTA LLENA");
     }
-    size++;
 }
 
+//se llama a template
 template <class T>
 T Lista<T>::erase(){
-    if(size!=0){
+    if(size!=0){ //si el tamaño no es 0 se elimina y despliega dato final
         T lastItem = data[size-1];
-        data.pop_back();
+        data.pop_back(); //se elimina el ultimo elemento
+        size--; //se reduce el tamaño
         return lastItem;
     }
     else{
         throw runtime_error("NO HAY ELEMENTOS");
     }
-    size--;
 }
 
+//se llama a template
 template <class T>
 T Lista<T>::getData(int index){
     if(index >= 0 && index < size && size!=0){
-        return data[index];
+        return data[index]; //si el indice esta entre el rango y el tamaño es mayor a 0
     }
     else{
-        if(size ==0){
+        if(size ==0){//si el tamaño es 0 entonces regresa error
             throw runtime_error("NO HAY ELEMENTOS");
         }
-        else{
+        else{//si la posicion es out of bounds regresa error
             throw runtime_error("POSICION INVALIDA");
         }
     }
 }
 
+//se llama a template
 template <class T>
 int Lista<T>::getSize(){
-    return size;
+    return size; //regresa el size
 }
 
+//se llama a template
 template <class T>
 void Lista<T>::print(){
-    for(int i= 0; i<size;i++){
-        cout<<"["<<i<<"]"<<" - "<<data[i]<<endl;
+    if(size !=0){//si no esta vacio
+        for(int i= 0; i<size;i++){//imprime desde 0 hasta el size
+            cout<<"["<<i<<"]"<<" - "<<data[i]<<endl;
+        }
+    }
+    else{
+        cout<<"NO HAY ELEMENTOS"<<endl;
     }
 }
 
