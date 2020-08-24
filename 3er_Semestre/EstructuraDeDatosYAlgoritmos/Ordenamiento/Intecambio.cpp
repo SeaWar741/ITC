e69b717de875462f41b4579f3eecad368c8513c6
SeaWar741 <juanca741@gmail.com>
2020-08-24 11:56:25 -0500

    added sequential search

--- 3er_Semestre/EstructuraDeDatosYAlgoritmos/Ordenamiento/Intecambio.cpp
+++ 3er_Semestre/EstructuraDeDatosYAlgoritmos/Ordenamiento/Intecambio.cpp
@@ -50,6 +50,30 @@ vector<T> bubble(vector<T> list){
 }
 
 //O(n^2)
+//Bubble sort mejorado
+template <class T>
+vector<T> bubble2(vector<T> list){
+    T temp;
+    bool continues;
+    int minIndex,comparaciones = 0,intercambios = 0;
+    for (int i = 0; i < list.size()-1&&continues; i++){
+        continues = false;
+        for (int j = 0; j < list.size()-1-i; j++){
+            if(list[j+i]<list[j]){
+                intercambios++;
+
+                temp=list[j];
+                list[j]=list[j+1];
+                list[j+1]=temp;
+            }
+        }
+        
+    }
+    cout<<"-Comparaciones: "<<comparaciones<<"\n-Intercambios: "<<intercambios<<endl<<endl;
+    return list;
+}
+
+//O(n^2)
 //Selection sort
 template <class T>
 vector<T> selection(vector<T> list){
@@ -72,7 +96,45 @@ vector<T> selection(vector<T> list){
     return list;
 }
 
+//O(n^2)
+//insercion
+template <class T>
+vector<T> insertion(vector<T> list)  {  
+    T key;
+    int i, j;
+    int comparaciones = 0,intercambios = 0;  
+    for (i = 1; i < list.size(); i++) {  
+        key = list[i];  
+        j = i - 1;  
 
+        comparaciones++;//aqui va?   
+
+        while (j >= 0 && list[j] > key){//esto cambiarlo
+             
+            list[j + 1] = list[j];  
+            j--;  
+        }
+        intercambios++;//aqui va?   
+        list[j + 1] = key;  
+    }
+    cout<<"-Comparaciones: "<<comparaciones<<"\n-Intercambios: "<<intercambios<<endl<<endl;
+    return list;  
+}  
+
+
+//--------------------Busquedas----------------------------
+template <class T>
+int sequential(vector<T> list,T element){
+    int i=0;
+    while(i<list.size()){
+        if(list[i]==element){
+            return i;
+        }
+        i++;
+    }
+    return -1;
+    
+}
 
 //O(n)
 //Imprimir vector
@@ -85,32 +147,56 @@ void print(vector<T> list){
 }
 
 int main(){
-    vector<int> list = {1,3,4,6,8,0,2,-4,6,15,10,25,20,21,30,32,-1,-6,2,45,-10,9,5,-45,-2,56,20,-21,-23,-19,20,21,-32,-5,2,30,-40,-23,-19};
-    
-    cout<<"Original:"<<endl;
+    //vector<int> list = {1,3,4,6,8,0,2,-4,6,15,10,25,20,21,30,32,-1,-6,2,45,-10,9,5,-45,-2,56,20,-21,-23,-19,20,21,-32,-5,2,30,-40,-23,-19};
+    vector<int> list ={29,3,8,17,30,15,42};
+
+    cout<<"*Original:"<<endl;
     print(list);
 
-    cout<<endl<<"Ordenada con Intercambio:"<<endl;
+    cout<<endl<<"*Ordenada con Intercambio:"<<endl;
     auto start = high_resolution_clock::now(); 
     print(exchange(list));
     auto stop = high_resolution_clock::now(); 
     auto duration = duration_cast<microseconds>(stop - start); 
     cout << "Tiempo de ejecucion: "<< duration.count()*0.000001 << " segundos" << endl; 
 
-    cout<<endl<<"Ordenada con Bubble:"<<endl;
+    cout<<endl<<"*Ordenada con Bubble:"<<endl;
     auto start1 = high_resolution_clock::now(); 
     print(bubble(list));
     auto stop1 = high_resolution_clock::now(); 
     auto duration1 = duration_cast<microseconds>(stop1 - start1); 
     cout << "Tiempo de ejecucion: "<< duration1.count()*0.000001 << " segundos" << endl; 
 
-
-    cout<<endl<<"Ordenada con Selection:"<<endl;
+    cout<<endl<<"*Ordenada con Bubble optimizado:"<<endl;
     auto start2 = high_resolution_clock::now(); 
-    print(selection(list));
+    print(bubble(list));
     auto stop2 = high_resolution_clock::now(); 
     auto duration2 = duration_cast<microseconds>(stop2 - start2); 
     cout << "Tiempo de ejecucion: "<< duration2.count()*0.000001 << " segundos" << endl;
+
+    cout<<endl<<"*Ordenada con Selection:"<<endl;
+    auto start3 = high_resolution_clock::now(); 
+    print(selection(list));
+    auto stop3 = high_resolution_clock::now(); 
+    auto duration3 = duration_cast<microseconds>(stop3 - start3); 
+    cout << "Tiempo de ejecucion: "<< duration3.count()*0.000001 << " segundos" << endl;
+
+    cout<<endl<<"*Ordenada con Insertion:"<<endl;
+    auto start4 = high_resolution_clock::now(); 
+    print(insertion(list));
+    auto stop4 = high_resolution_clock::now(); 
+    auto duration4 = duration_cast<microseconds>(stop4 - start4); 
+    cout << "Tiempo de ejecucion: "<< duration4.count()*0.000001 << " segundos" << endl;
+
+
+    //-------------------Busquedas-----------------------------------------------------
+
+    cout<<endl<<"*Busqueda iterativa:"<<endl;
+    auto start5 = high_resolution_clock::now();  
+    cout<<sequential(list,30)<<endl;
+    auto stop5 = high_resolution_clock::now(); 
+    auto duration5 = duration_cast<microseconds>(stop5 - start5); 
+    cout << "Tiempo de ejecucion: "<< duration4.count()*0.000001 << " segundos" << endl;
 
     return 0;
 }
