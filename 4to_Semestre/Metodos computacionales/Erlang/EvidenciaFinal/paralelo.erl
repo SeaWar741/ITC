4a97911ebaf47f9de7a883f8267b314b8817e86b
seawar741 <juanca741@gmail.com>
2021-06-10 20:17:21 -0500

    added final iteration metodos

--- 4to_Semestre/Metodos computacionales/Erlang/EvidenciaFinal/paralelo.erl
+++ 4to_Semestre/Metodos computacionales/Erlang/EvidenciaFinal/paralelo.erl
@@ -1,5 +1,5 @@
 -module(paralelo).
--export([readFile/1]).
+-export([readFile/1, readFileSec/1, readFileParallel/1, readFileWithTimer/1]).
 -export([performOperation/1]).
 -export([checkOperation/3]).
 
@@ -31,7 +31,7 @@ performOperation(Op) ->
 
 %Funcion secuencial
 readFile(File) ->
-  FileIn = File++".in",
+  FileIn = File ++ ".in",
   FileOut = File ++ ".out",
   {ok,Binary} = file:read_file(FileIn),
   List1 = [binary_to_list(Bin) || Bin <- binary:split(Binary,<<"\n">>,[global]),Bin =/= << >>],
@@ -40,9 +40,41 @@ readFile(File) ->
   Print = [string:join(Data, LineSep), LineSep],
   file:write_file(FileOut, Print).
 
+readFileWithTimer(File)->
+    statistics(runtime),
+    statistics(wall_clock),
+    
+    readFile(File),
+
+    {_, Time1} = statistics(runtime),
+    {_, Time2} = statistics(wall_clock),
+    U1 = Time1 * 1000,
+    U2 = Time2 * 1000,
+    io:format("\nTiempo de Ejecucion: ~p (~p) microseconds~n",
+    [U1,U2]).
+
+%Funcion secuencial
+runSequencial(File) ->
+  File1 = File ++ "1",
+  File2 = File ++ "2",
+  File3 = File ++ "3",
+  File4 = File ++ "4",
+  readFileWithTimer(File1),
+  readFileWithTimer(File2),
+  readFileWithTimer(File3),
+  readFileWithTimer(File4).
+
 
 %Funcion en paralelo
-%readFileParallel(File) ->
+runParallel (File) ->
+  File1 = File ++ "1",
+  File2 = File ++ "2",
+  File3 = File ++ "3",
+  File4 = File ++ "4",
+  spawn(paralelo, readFileWithTimer, [File1]),
+  spawn(paralelo, readFileWithTimer, [File2]),
+  spawn(paralelo, readFileWithTimer, [File3]),
+  spawn(paralelo, readFileWithTimer, [File4]).
 
 
 
